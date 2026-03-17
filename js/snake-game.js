d0d5c8efc4a8556c87b98da5a41ac89af1f62282
Ivan Hrynkiv <ivan.hrynkiv@gmail.com>
2026-03-17 16:09:35 +0200

    fix speed

--- js/snake-game.js
+++ js/snake-game.js
@@ -14,7 +14,7 @@ let snakeGame = {
     direction: { x: 0, y: 0 },
     score: 0,
     gameActive: false,
-    gameSpeed: 195,
+    gameSpeed: 234,
     lastDirection: { x: 0, y: 0 },
     touchStartX: 0,
     touchStartY: 0,
@@ -385,7 +385,7 @@ function initializeSnakeGame() {
     snakeGame.score = 0;
     snakeGame.gameActive = false;
     snakeGame.justAte = false;
-    snakeGame.gameSpeed = 195;
+    snakeGame.gameSpeed = 234;
     
     generateFood();
     console.log('Game initialized. Snake at:', snakeGame.snake[0], 'TileCount:', snakeGame.tileCount);
@@ -703,7 +703,7 @@ function eatFood() {
     generateFood();
 
     // Increase speed slightly
-    if (snakeGame.gameSpeed > 104) {
+    if (snakeGame.gameSpeed > 125) {
         snakeGame.gameSpeed -= 3;
         stopSnakeGameLoop();
         startSnakeGameLoop();
