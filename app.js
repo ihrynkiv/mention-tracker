b86cf64f535d3da6ecd03b030572a871f450db1c
Ivan Hrynkiv <ivan.hrynkiv@gmail.com>
2026-04-08 17:22:15 +0300

    block web site

--- app.js
+++ app.js
@@ -871,15 +871,6 @@ async function loadTodayStatus() {
 
 async function loadDailyLegend() {
     try {
-        const trackingContent = document.querySelector('.tracking-content');
-
-        // Always hide tracking UI and show sad static message
-        if (trackingContent) {
-            trackingContent.querySelector('h2').style.display = 'none';
-            trackingContent.querySelector('.yes-button').style.display = 'none';
-            trackingContent.querySelector('#cooldownMessage').style.display = 'none';
-        }
-
         // Show sad static message with total stats
         await showSadStaticMessage();
 
